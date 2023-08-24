7efa793e2b9cea625ccddfb572cf1be7662a2c24
NdatimanaSamuel <ndatimanasamuel1@gmail.com>
2023-08-24 16:33:17 +0200

    chore : setting up linting

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	singleQuote: true,
+	trailingComma: 'es5',
+	endOfLine: 'lf',
+	printWidth: 80,
+	trailingComma: 'es5',
+	semi: false,
+	jsxSingleQuote: true,
+	singleQuote: true,
+	useTabs: true,
+	tabWidth: 2,
+	arrowParens: 'always',
+}
